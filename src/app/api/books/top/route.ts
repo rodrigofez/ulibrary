c9599a93281dc3b2e2a3ee4b14013f75beb55e81
Rodrigo Lopez <rodrigolp0521@gmail.com>
2023-11-03 10:44:33 -0600

    refactor: revalidation

--- src/app/api/books/top/route.ts
+++ src/app/api/books/top/route.ts
@@ -1,10 +1,9 @@
 import { prisma } from "@/lib/db";
 import { NextApiResponse } from "next";
 import { NextResponse } from "next/server";
+import { ZodError } from "zod";
 
 export const GET = async (req: Request, res: NextApiResponse) => {
-  console.log("Hello World");
-
   const books = await prisma.book.findMany({
     orderBy: {
       timesBorrowed: "desc",
@@ -13,4 +12,18 @@ export const GET = async (req: Request, res: NextApiResponse) => {
   });
 
   return NextResponse.json({ data: books });
+};
+
+export const POST = async (req: Request, res: Response) => {
+  try {
+    const body = await req.json();
+
+    return NextResponse.json({ data: body });
+  } catch (e: any) {
+    if (e instanceof ZodError) {
+      return NextResponse.json({ error: e.issues }, { status: 400 });
+    }
+
+    return NextResponse.json({ error: e.message }, { status: 500 });
+  }
 };
